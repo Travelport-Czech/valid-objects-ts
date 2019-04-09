d8135a634aecfc28d94d586d0d46fd5e8cf65e81
Michal Bureš <michal.ezop@gmail.com>
2019-04-09 10:18:51 +0200

    Add excluded chars to ValidEmail

--- src/validObjects/ValidEmail.ts
+++ src/validObjects/ValidEmail.ts
@@ -3,7 +3,7 @@ import { ValidObjectError } from '@/errors/ValidObjectError'
 import { ValidString } from '@/validObjects/ValidString'
 import { validate } from 'email-validator'
 
-export const validateEmail = (email: string | undefined): boolean => {
+export const validateEmail = (email: string | undefined, excludeChars: string[]): boolean => {
   if (!email) {
     return false
   }
@@ -12,12 +12,26 @@ export const validateEmail = (email: string | undefined): boolean => {
     return false
   }
 
+  const foundChar = excludeChars.reduce((accumulator: boolean, currentValue: string): boolean => {
+    if (accumulator) {
+      return true
+    }
+    if (email.includes(currentValue)) {
+      return true
+    }
+    return false
+  }, false)
+
+  if (foundChar) {
+    return false
+  }
+
   return true
 }
 
 export class ValidEmail extends ValidString {
   // tslint:disable-next-line:no-any
-  constructor(val: any) {
+  constructor(val: any, excludeChars: string[]) {
     try {
       super(val)
     } catch (err) {
@@ -27,7 +41,7 @@ export class ValidEmail extends ValidString {
       throw new InvalidEmailError(err.message)
     }
 
-    if (!validateEmail(this.value)) {
+    if (!validateEmail(this.value, excludeChars)) {
       throw new InvalidEmailError(this.value)
     }
   }
