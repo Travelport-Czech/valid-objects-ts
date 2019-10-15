e796a52d8fc9ec59d898aceef4bd4fa83366ce02
Michal Bureš <michal.bures@travelportgds.cz>
2019-10-15 09:20:49 +0200

    Rename dist

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const alias = require('path-alias-resolver/gulp');
 
 gulp.task('default', () => {
   return gulp.src([
-    '.dist/**/*d.ts',
-    '.dist/**/*.js'
+    'dist/**/*d.ts',
+    'dist/**/*.js'
   ])
     .pipe(alias('.', {
       '@': './'
     }))
-    .pipe(gulp.dest('.dist'));
+    .pipe(gulp.dest('dist'));
 });
